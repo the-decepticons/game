240db39544468835d1e9e3dd7b9c298f7dbcedbd
hemant-hari <42181620+hemant-hari@users.noreply.github.com>
2019-03-09 02:56:55 +0000

    Added beginnings of pick up and drop functionality
    Pickup script is now functional, additional work needs to be done to the drop functionality. Currently it simply destroys the held object. More to come later.

---
+++ Assets/Scripts/PickupScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{
+    private bool interactableInRange = false;
+    private bool itemHeld = false;
+    private GameObject interactObject;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && interactableInRange && !itemHeld)
+        {
+            pickUpCurrentObject();
+            itemHeld = true;
+        }
+
+        if (Input.GetButtonDown("Fire2") && itemHeld)
+        {
+            Destroy(gameObject.transform.Find("TestVeg(Clone)").gameObject);
+            itemHeld = false;
+        }
+    }
+    
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.name.Equals("TestVeg")){
+            interactableInRange = true;
+            interactObject = coll.gameObject;
+        }
+    }
+
+    void pickUpCurrentObject()
+    {
+        Instantiate(interactObject, gameObject.transform.GetChild(3).position, new Quaternion(), this.gameObject.transform);
+    }
+}
