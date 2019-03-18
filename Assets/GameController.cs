194e92d7c0b649dd1d516398d990f2c699fff91a
hemant-hari <42181620+hemant-hari@users.noreply.github.com>
2019-03-18 23:28:45 +0000

    commit to fix local changes

---
+++ Assets/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public List<string> input;
+    public List<string> soln;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = new List<string>();
+        setSolution();
+    }
+
+    void setSolution()
+    {
+        soln.Add("move_forward()");
+        soln.Add("move_forward()");
+        soln.Add("move_forward()");
+        soln.Add("move_forward()");
+        soln.Add("move_forward()");
+        soln.Add("open()");
+    }
+
+    // Update is called once per frame
+    public void checkWin()
+    {
+        int samecnt = 0;
+
+        for(int i=0; i<(input.Count); i++)
+        {
+            if (i < soln.Count)
+            {
+                if (soln[i].Equals(input[i]))
+                {
+                    samecnt++;
+                }
+            }
+        }
+
+        if ( samecnt == input.Count && input.Count == soln.Count )
+        {
+            GameObject content = GameObject.Find("Content");
+            GameObject textBox = content.gameObject.transform.GetChild(0).gameObject;
+            Text code = textBox.GetComponent<Text>() as Text;
+            code.text += "Success!" + "\n";
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/interactFruit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactFruit : MonoBehaviour
+{
+    public GameObject newObject;
+    public Transform newPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // to do: on click, instance new object at target position, destroy original object
+    void OnMouseDown()
+    {
+        Instantiate(newObject, newPosition.position, Quaternion.identity);
+    }
+}
