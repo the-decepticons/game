3368bbc20e115efb5883352e69856ae48074b2b4
Mickey K <mickeykkim@gmail.com>
2019-03-20 20:04:39 +0000

    idle and running animations implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,8 @@ public class PlayerMovement : MonoBehaviour
     private string m_MovementAxisName;
     private string m_TurnAxisName;
     private Rigidbody m_Rigidbody;
-    private float m_MovementInputValue;
-    private float m_TurnInputValue;
 
+    static Animator anim;
 
     private void Awake()
     {
@@ -23,50 +22,32 @@ public class PlayerMovement : MonoBehaviour
     private void OnEnable()
     {
         m_Rigidbody.isKinematic = false;
-        m_MovementInputValue = 0f;
-        m_TurnInputValue = 0f;
     }
 
     private void Start()
     {
         m_MovementAxisName = "Vertical";
         m_TurnAxisName = "Horizontal";
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        // Store the player's input and make sure the audio for the engine is playing.
-        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
-        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
-    }
-    
-    private void FixedUpdate()
-    {
-        // Move and turn the tank.
-        Move();
-        Turn();
-    }
+        float translation = Input.GetAxis(m_MovementAxisName) * m_Speed;
+        float rotation = Input.GetAxis(m_TurnAxisName) * m_TurnSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
 
-
-    private void Move()
-    {
-        // Adjust the position of the tank based on the player's input.
-
-        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
-
-        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-    }
-
-
-    private void Turn()
-    {
-        // Adjust the rotation of the tank based on the player's input.
-
-        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
-
-        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-
-        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+        if (translation > 0 || translation < 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
     }
 }
    
