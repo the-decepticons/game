8dc5ba674c01c1c70025eee4cdd1cff9bca51e91
Mickey K <mickeykkim@gmail.com>
2019-03-20 12:49:07 +0000

    created some assets and spawn positions

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text m_MessageText;                  // Reference to the overlay Text to display winning text, etc.
+    public GameObject m_Player;                 // Reference to the prefab the players will control.
+    public FruitManager[] m_Fruit;              // A collection of managers for enabling and disabling different aspects of the tanks.
+    public CharacterManager m_Character;        // A collection of managers for enabling and disabling different aspects of the tanks.
+
+
+}
