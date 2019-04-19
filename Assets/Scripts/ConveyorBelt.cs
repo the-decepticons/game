b7f605ecdb9752f76d940e2546af64046cb8b8d6
Mickey K <mickeykkim@gmail.com>
2019-04-19 18:56:17 +0100

    git ignore update

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -10,6 +10,13 @@ public class ConveyorBelt : MonoBehaviour
     private Vector3 displacement;
     private float currentScroll;
 
+
+    //to store x coord of collision and code of fruit possibly not the most efficent way?
+    private Dictionary<float, string> foodOrderConveyor =
+            new Dictionary<float, string>();
+    
+ 
+ 
     private void Update()
     {
         // Scroll texture to fake it moving
@@ -18,13 +25,48 @@ public class ConveyorBelt : MonoBehaviour
     }
 
     // This function repeats as long as the object is touching
-    private void OnCollisionStay(Collision otherThing)
-    {
-        // move objects each frame relative to itemSpeed * original position
-        displacement = transform.forward * beltSpeed * itemFactor * Time.deltaTime;
-        otherThing.rigidbody.MovePosition(otherThing.rigidbody.position + displacement);
+     private void OnCollisionStay(Collision otherThing)
+     {
+         // move objects each frame relative to itemSpeed * original position
+         displacement = transform.forward * beltSpeed * itemFactor * Time.deltaTime;
+         otherThing.rigidbody.MovePosition(otherThing.rigidbody.position + displacement);
 
-        // this adds force instead of displacement but has unexpected behaviour:
-        // otherThing.rigidbody.AddForce(displacement / Time.deltaTime, ForceMode.Acceleration);
+         // this adds force instead of displacement but has unexpected behaviour:
+         // otherThing.rigidbody.AddForce(displacement / Time.deltaTime, ForceMode.Acceleration);
+     }
+     
+    void OnCollisionEnter(Collision col)
+    {
+        // not needed as playervariant and playchar are both kinematic objects not driven by physics engine
+        //if (col.gameObject.name != playervariant)
+        //if (col.gameObject.tag == "the tag of your target"){
+        //Make stuff here. add fruit tag to all the food?
+    Debug.Log(this.gameObject.name + " has collided with " + col.gameObject.name);
+        //quick fix turn on iskinematic when collides so fruit doesnt role around - this will also apply to player variant probably do something in the fruit object rather than this 
+        col.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+        //get contacts from collision and get x out of this 
+        ContactPoint contact = col.GetContact(0);
+
+       //this is how you'd get rotation Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        Debug.Log(pos.x);
+        //Use tags in future? if col.gameobject.tag = food/fruit then proceed
+        GameObject food1 = col.gameObject;
+
+       // foodOrderConveyor.Add(pos.x, food1.GetComponent<BananaScript>().foodCode);
+
+
+        //i want to nly be able to put it in once, so must be romved every time its picked up, then re inserted in a way that both sorts it and preserves order
+        //!!!!! START HERE 
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(this.gameObject.name + " has triggered " + other.gameObject.name);
     }
 }
+
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
+
 {
     public Text m_MessageText;
+    public Text m_QuestionText;
     public GameObject m_PlayerObject;
     public List<GameObject> m_FoodObject;
     public GameObject m_CharacterSpawnLocation;
@@ -14,15 +17,92 @@ public class GameManager : MonoBehaviour
     public CharacterManager m_CharacterManager;
     public FoodManager[] m_FoodManager;
     public int m_GameLevel;
+    //set in inspector
+    public SimpleObjectPool bananaObjectPool;
+    //my stuff
+    private DataController dataContoller;
+    private LevelData currentLevelData;
+    private QuestionData question;
+    private SolutionData solution;
+    private FruitCodeData[] fruits;
+    private bool isLevelActive;
+    private List<GameObject> bananaGameObjects = new List<GameObject>();
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnCharacter();
-        SpawnFood();
+        //store ref to DC
+        dataContoller = FindObjectOfType<DataController>(); // risks associated with usinf ind object of type but can be confident datacontroller to be there because we start from persistent scene
+        Debug.Log(dataContoller);
+        currentLevelData = dataContoller.GetCurrentLevelData();
+        Debug.Log(currentLevelData.name);
+        // question 
+        question = currentLevelData.levelQuestion;
+        //fruits in a level
+        fruits = currentLevelData.levelFruits;
 
+        
+        
+        SpawnCharacter();
+
+        SpawnFruit();
+        //SpawnFood();
+        //isroundactive = true;
         // StartCoroutine(GameLoop());
+    }
+
+    //first thing we want to do is show title in screen
+    //then instantiate fruit, player, show question in UI and also show fruit data displayed for this level 
+    //could use display of title to make sure fruit elements are got rid of?
+
+    private void ShowLevelTitle()
+    {
+        //not really necessary but good methodology for when fruit will be removed by oven
+        RemoveFruit();
+        //display currentLevelData.name
+
+    }
+
+    private void SpawnFruit()
+    {
+       // m_QuestionText.text = question.questionText;
+        //Instantiate fruit that are parts of the level
+        
+        for (int i = 0; i < currentLevelData.levelFruits.Length; i++)
+            
+        {   //get banana from object pool and place at spawn location array position
+            GameObject bananaGameObject = bananaObjectPool.GetObject();
+            
+            bananaGameObject.GetComponent<Transform>().position = m_FoodSpawnLocation[i].transform.position;
+            bananaGameObject.GetComponent<Transform>().rotation = m_FoodSpawnLocation[i].transform.rotation;
+            bananaGameObject.GetComponent<PickUp>().theDest = GameObject.Find("HoldPosition").transform;
+
+            //iterate through levelfruits passing data into the setup of banana prefab
+            bananaGameObject.GetComponent<FruitScript>().SetUp(currentLevelData.levelFruits[i]);
+            bananaGameObjects.Add(bananaGameObject);
+            
+
+
+            //does this change the object in bananagameobjects? is it the same as doing before add to array?
+
+            //get banana thats not being used and set it up
+
+            //spawn at random point List<Product> products = GetProducts();
+            //products.Shuffle();
+
+        }
+    }
+
+    private void RemoveFruit()
+    {//foodobjects
+        while (bananaGameObjects.Count > 0)
+        {
+            //send back to object pool to be ready to be reused and then remove from list of active fruit game objects 
+            bananaObjectPool.ReturnObject(bananaGameObjects[0]);
+            bananaGameObjects.RemoveAt(0);
+        }
     }
 
 
@@ -32,27 +112,37 @@ public class GameManager : MonoBehaviour
     }
 
 
+
     private void SpawnCharacter()
     {
         Instantiate(m_PlayerObject, m_CharacterSpawnLocation.transform.position, m_CharacterSpawnLocation.transform.rotation);
     }
 
-
+    /*
     private void SpawnFood()
     {
-        Instantiate(m_FoodObject[(int)FoodType.avocado], m_FoodSpawnLocation[0].transform.position, m_FoodSpawnLocation[0].transform.rotation);
-        Instantiate(m_FoodObject[(int)FoodType.banana], m_FoodSpawnLocation[1].transform.position, m_FoodSpawnLocation[1].transform.rotation);
-        Instantiate(m_FoodObject[(int)FoodType.mushroom], m_FoodSpawnLocation[2].transform.position, m_FoodSpawnLocation[2].transform.rotation);
-        Instantiate(m_FoodObject[(int)FoodType.onion], m_FoodSpawnLocation[3].transform.position, m_FoodSpawnLocation[3].transform.rotation);
-        Instantiate(m_FoodObject[(int)FoodType.pepper], m_FoodSpawnLocation[4].transform.position, m_FoodSpawnLocation[4].transform.rotation);
-        Instantiate(m_FoodObject[(int)FoodType.tomato], m_FoodSpawnLocation[5].transform.position, m_FoodSpawnLocation[5].transform.rotation);
-    }
 
+        //Instantiate food object and set transform upon holding to holdposition
+        GameObject food1 = Instantiate(m_FoodObject[(int)FoodType.avocado], m_FoodSpawnLocation[0].transform.position, m_FoodSpawnLocation[0].transform.rotation);
+        food1.GetComponent<PickUp>().theDest = GameObject.Find("HoldPosition").transform;
 
-    /*
-    IEnumerator GameLoop()
-    {
+        GameObject food2 = Instantiate(m_FoodObject[(int)FoodType.banana], m_FoodSpawnLocation[1].transform.position, m_FoodSpawnLocation[1].transform.rotation);
+        food2.GetComponent<PickUp>().theDest = GameObject.Find("HoldPosition").transform;
 
-    }
-    */
+        GameObject food3 = Instantiate(m_FoodObject[(int)FoodType.mushroom], m_FoodSpawnLocation[2].transform.position, m_FoodSpawnLocation[2].transform.rotation);
+        food3.GetComponent<PickUp>().theDest = GameObject.Find("HoldPosition").transform;
+        GameObject food4 = Instantiate(m_FoodObject[(int)FoodType.onion], m_FoodSpawnLocation[3].transform.position, m_FoodSpawnLocation[3].transform.rotation);
+        food4.GetComponent<PickUp>().theDest = GameObject.Find("HoldPosition").transform;
+        GameObject food5 = Instantiate(m_FoodObject[(int)FoodType.pepper], m_FoodSpawnLocation[4].transform.position, m_FoodSpawnLocation[4].transform.rotation);
+        food5.GetComponent<PickUp>().theDest = GameObject.Find("HoldPosition").transform;
+        GameObject food6 = Instantiate(m_FoodObject[(int)FoodType.tomato], m_FoodSpawnLocation[5].transform.position, m_FoodSpawnLocation[5].transform.rotation);
+        food6.GetComponent<PickUp>().theDest = GameObject.Find("HoldPosition").transform;
+
+        can put this all on one line of code but it looks like this.... 
+         Instantiate(m_FoodObject[(int)FoodType.avocado], m_FoodSpawnLocation[0].transform.position, m_FoodSpawnLocation[0].transform.rotation).GetComponent<PickUp>().theDest = GameObject.Find("HoldPosition").transform;
+        could maybe do some refactoring   }*/
+
 }
+    
+  
+
